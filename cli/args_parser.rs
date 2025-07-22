e3040e040d85e700009da930bd1cf3ad6d6a782e
karl <5895483+karlivory@users.noreply.github.com>
2025-07-22 10:33:35 +0300

    add cli client

--- cli/args_parser.rs
+++ cli/args_parser.rs
@@ -1,13 +1,4 @@
-// cli/argparse.rs
-//
-// responsibility: parsing args into a MainOp enum (?)
-//
-// I want stuff like
-// khttp server echo
-// khttp server upper 8080
-//
-// khttp get google.com
-// khttp post localhost:8080 -dfoobar
+// cli/args_parser.rs
 
 use std::env::Args;
 
@@ -16,22 +7,140 @@ use khttp::common::HttpMethod;
 pub struct ArgsParser {}
 
 impl ArgsParser {
-    pub fn parse(args: Args) -> MainOp {
-        todo!();
+    pub fn parse(mut args: Args) -> Result<MainOp, ArgsError> {
+        // skip first arg
+        args.next();
+        parse_main_op(args)
     }
 }
 
+fn parse_main_op(mut args: Args) -> Result<MainOp, ArgsError> {
+    let op = get_required_arg(&mut args)?;
+
+    match op.as_str() {
+        "server" => todo!(),
+        x => Ok(MainOp::Client(parse_client_op(x, args)?)),
+    }
+}
+
+fn parse_client_op(method: &str, mut args: Args) -> Result<ClientOp, ArgsError> {
+    let method = HttpMethod::from(method.to_uppercase().as_str());
+    if let HttpMethod::Custom(_) = method {
+        return Err(ArgsError::InvalidArgs("invalid method".to_string()));
+    }
+
+    let address = get_required_arg(&mut args)?;
+    let (host, port, uri) = parse_address(address)?;
+    let opt_args = parse_client_opt_arg(&mut args)?;
+
+    Ok(ClientOp {
+        method,
+        host,
+        port,
+        uri,
+        opt_args,
+    })
+}
+
+fn get_required_arg(args: &mut Args) -> Result<String, ArgsError> {
+    let arg = args.next();
+    if arg.is_none() {
+        return Err(ArgsError::InvalidArgs("foo".to_string()));
+    }
+    Ok(arg.unwrap())
+}
+
+fn parse_client_opt_arg(args: &mut Args) -> Result<Vec<ClientOpArg>, ArgsError> {
+    let mut opt_args = Vec::<ClientOpArg>::new();
+    loop {
+        let arg = args.next();
+        if arg.is_none() {
+            return Ok(opt_args);
+        }
+        let arg = arg.unwrap();
+
+        if arg == "-v" || arg == "--verbose" {
+            opt_args.push(ClientOpArg::Verbose);
+            continue;
+        }
+
+        if arg == "-d" || arg == "--data" {
+            // next arg is body
+            let body = get_required_arg(args)?;
+            opt_args.push(ClientOpArg::Body(body));
+            continue;
+        }
+
+        if arg == "-H" || arg == "--header" {
+            // next arg is header
+            let header = get_required_arg(args)?;
+            let h: Vec<&str> = header.splitn(2, ":").collect();
+            opt_args.push(ClientOpArg::Header((h[0].to_string(), h[1].to_string())));
+            continue;
+        }
+    }
+}
+
+fn parse_address(address: String) -> Result<(String, u16, String), ArgsError> {
+    // return address, port, uri
+    let mut port: u16 = 80;
+    let mut uri: String = String::from("/");
+
+    let parts: Vec<&str> = address.splitn(2, "/").collect();
+    if parts.len() > 1 {
+        uri = parts[1].to_string();
+    }
+
+    let host_parts: Vec<&str> = parts[0].splitn(2, ":").collect();
+    if host_parts.len() > 2 {
+        return Err(ArgsError::InvalidArgs(String::from(
+            "invalid address: too many :",
+        )));
+    }
+    if host_parts.len() == 2 {
+        port = host_parts[1]
+            .parse()
+            .map_err(|_| ArgsError::InvalidArgs("invalid port".to_string()))?;
+    }
+
+    Ok((host_parts[0].to_string(), port, uri))
+}
+
+#[derive(Debug)]
+pub enum ArgsError {
+    InvalidArgs(String),
+}
+
+#[derive(Debug)]
 pub enum MainOp {
     Server(ServerOp),
     Client(ClientOp),
 }
 
+#[derive(Debug)]
 pub enum ServerOp {
-    Echo,
-    Upper,
+    Echo(Vec<ServerOpArg>),
+    Upper(Vec<ServerOpArg>),
 }
 
+#[derive(Debug)]
+pub enum ServerOpArg {
+    Port(u16),
+    BindAddress(String),
+}
+
+#[derive(Debug)]
 pub struct ClientOp {
-    method: HttpMethod,
-    address: String,
+    pub method: HttpMethod,
+    pub host: String,
+    pub port: u16,
+    pub uri: String,
+    pub opt_args: Vec<ClientOpArg>,
+}
+
+#[derive(Debug)]
+pub enum ClientOpArg {
+    Header((String, String)), // -H or --header
+    Body(String),             // -d or --data
+    Verbose,                  // -v or --verbose
 }
--- cli/main.rs
+++ cli/main.rs
@@ -1,29 +1,72 @@
 // cli/main.rs
 
-use args_parser::ArgsParser;
+use std::env;
+use std::io::Cursor;
+
+use args_parser::{ArgsParser, ClientOp, ClientOpArg, MainOp};
+use khttp::client::Client;
 use khttp::common::{HttpHeaders, HttpMethod};
-use khttp::server::{App, HttpRequestContext};
+use khttp::server::App;
 
 pub mod args_parser;
 
 fn main() {
-    // let args = ArgsParser::parse(env::args());
-    // for argument in env::args() {
-    //     println!("{argument}");
-    // }
-    run_echo_server();
+    let arg_vec: Vec<String> = env::args().collect();
+    dbg!(arg_vec);
+    let args = ArgsParser::parse(env::args());
+    if args.is_err() {
+        print_help();
+    } else {
+        dbg!(&args);
+        handle_op(args.unwrap());
+    }
+}
+
+fn handle_op(op: MainOp) {
+    match op {
+        MainOp::Server(op) => todo!(),
+        MainOp::Client(op) => handle_client_op(op),
+    }
+}
+
+fn handle_client_op(op: ClientOp) {
+    let address = format!("{}:{}", op.host, op.port);
+    let client = Client::new(&address);
+    let mut headers = HttpHeaders::new();
+    let mut body = String::new();
+    let mut verbose = false;
+    for opt_arg in op.opt_args {
+        match opt_arg {
+            ClientOpArg::Header((h, v)) => headers.add_header(&h, &v),
+            ClientOpArg::Body(b) => body = b,
+            ClientOpArg::Verbose => verbose = true,
+        };
+    }
+    let mut response = client
+        .exchange(&op.method, &op.uri, &headers, Cursor::new(body))
+        .expect("TODO");
+    let response_body = response.read_body_to_string();
+    if verbose {
+        println!("{} {}", response.status.code, response.status.reason);
+        for (h, v) in response.headers.get_header_map() {
+            println!("{}: {}", h, v);
+        }
+    }
+    print!("{}", response_body);
+}
+
+fn print_help() {
+    println!("-- khttp client");
+    println!();
+    println!("HELP: how to use and stuff");
+    println!("example: khttp get foo");
 }
 
 fn run_echo_server() {
     let mut app = App::new(8080, 5);
-    app.map_route(HttpMethod::Post, "/foo", |mut ctx, res| {
-        let mut headers = HttpHeaders::new();
-        if let Some(len) = ctx.headers.get_content_length() {
-            headers.set_content_length(len);
-        }
-        res.ok(&headers, ctx.read_body().to_ascii_uppercase().as_slice());
+    app.map_route(HttpMethod::Post, "/echo", |mut ctx, res| {
+        let body = ctx.read_body().to_ascii_uppercase();
+        res.ok(&ctx.headers, body.as_slice());
     });
-    app.serve_n(1);
+    app.serve();
 }
-
-fn foo(mut context: HttpRequestContext) {}
