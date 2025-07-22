f44a9a834db1a410252a35f96cc1075c809c3baa
karl <5895483+karlivory@users.noreply.github.com>
2025-07-22 13:03:37 +0300

    update integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -7,22 +7,35 @@ mod tests {
         common::{HttpHeaders, HttpMethod, HttpStatus},
         server::App,
     };
-    use std::{io::Cursor, thread, time::Duration};
+    use std::{
+        io::Cursor,
+        thread::{self},
+        time::Duration,
+    };
 
     #[test]
     fn simple_multi_test() {
         // start server
         let h = thread::spawn(|| {
             let mut app = App::new(8080, 3);
-            app.map_route(HttpMethod::Post, "/to-upper", move |mut ctx, res| {
+
+            app.map_route(HttpMethod::Get, "/hello", move |_, res| {
+                let hello = "Hello, World!".to_string();
+                let mut headers = HttpHeaders::new();
+                headers.set_content_length(hello.len());
+                res.send(&HttpStatus::of(200), &headers, Cursor::new(hello));
+            });
+
+            app.map_route(HttpMethod::Post, "/api/uppercase", move |mut ctx, res| {
                 let mut headers = HttpHeaders::new();
                 headers.set_content_length(ctx.headers.get_content_length().unwrap());
 
                 let b = ctx.read_body_to_string().to_ascii_uppercase();
 
-                res.send(&HttpStatus::of(200), &headers, Cursor::new(b));
+                res.send(&HttpStatus::of(201), &headers, Cursor::new(b));
             });
-            app.serve_n(2);
+
+            app.serve_n(3);
         });
         // wait for server to be active
         thread::sleep(Duration::from_millis(10));
@@ -30,32 +43,27 @@ mod tests {
         // init client
         let client = Client::new("localhost:8080");
 
-        // test 1 : echo
+        // test: GET /hello
+        let mut response = client.get("/hello", &HttpHeaders::new()).unwrap();
+        assert_eq!(response.status.code, 200);
+        assert_eq!(response.read_body_to_string(), "Hello, World!");
+
+        // test: POST /api/uppercase
         let mut response = client
             .post(
-                "/to-upper",
+                "/api/uppercase",
                 &HttpHeaders::from(vec![(HttpHeaders::CONTENT_LENGTH, "7")]),
                 Cursor::new("test123"),
             )
             .unwrap();
+        assert_eq!(response.status.code, 201);
         assert_eq!(response.read_body_to_string(), "TEST123");
 
-        // test 2 : check for 404
+        // test for 404
         let response = client
             .post("/not-routed", &HttpHeaders::new(), Cursor::new(""))
             .unwrap();
         assert_eq!(response.status.code, 404);
-
-        // test 3 : check for 500
-        // let response = client
-        //     .exchange(HttpRequest {
-        //         body: None,
-        //         headers: Default::default(),
-        //         method: HttpMethod::Custom("FOOBAR".to_string()),
-        //         uri: "/".to_string(),
-        //     })
-        //     .unwrap();
-        // assert_eq!(response.status.code, 500);
 
         // wait for server thread to finish
         let _ = h.join();
