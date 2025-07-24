e71fc431374c7def3ee1137be189adc9270b6022
karl <5895483+karlivory@users.noreply.github.com>
2025-07-25 02:23:56 +0300

    improve integration test

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,5 +1,3 @@
-// tests/integration.rs
-
 #[cfg(feature = "client")]
 #[cfg(test)]
 mod tests {
@@ -14,60 +12,72 @@ mod tests {
         time::Duration,
     };
 
+    const TEST_PORT: u16 = 32734;
+
     #[test]
     fn simple_multi_test() {
-        // start server
-        let h = thread::spawn(|| {
-            let mut app = App::new("127.0.0.1", 8080);
-
-            app.map_route(HttpMethod::Get, "/hello", move |_, res| {
-                let hello = "Hello, World!".to_string();
-                let mut headers = HttpHeaders::new();
-                headers.set_content_length(hello.len() as u64);
-                res.send(&HttpStatus::OK, headers, Cursor::new(hello));
-            });
-
-            app.map_route(HttpMethod::Post, "/api/uppercase", move |mut ctx, res| {
-                let mut headers = HttpHeaders::new();
-                headers.set_content_length(ctx.headers.get_content_length().unwrap());
-
-                let b = ctx.read_body_to_string().unwrap().to_ascii_uppercase();
-
-                res.send(&HttpStatus::of(201), headers, Cursor::new(b));
-            });
-
-            app.serve_n(3).ok();
-        });
-        // wait for server to be active
+        // start server, wait for it to be active
+        let h = start_server(4);
         thread::sleep(Duration::from_millis(10));
 
-        // init client
-        let client = Client::new("localhost:8080");
+        let client = Client::new(&format!("localhost:{}", TEST_PORT));
 
-        // test: GET /hello
-        let mut response = client.get("/hello", HttpHeaders::new()).unwrap();
-        assert_eq!(response.status.code, 200);
-        assert_eq!(response.read_body_to_string().unwrap(), "Hello, World!");
+        let response = client.get("/hello", HttpHeaders::new()).unwrap();
+        assert_status_and_body(response, 200, "Hello, World!");
 
-        // test: POST /api/uppercase
-        let mut response = client
-            .post(
-                "/api/uppercase",
-                HttpHeaders::from(vec![(HttpHeaders::CONTENT_LENGTH, "7")]),
-                Cursor::new("test123"),
-            )
+        let response = client
+            .post("/api/uppercase", HttpHeaders::new(), Cursor::new("test123"))
             .unwrap();
-        assert_eq!(response.status.code, 201);
-        assert_eq!(response.read_body_to_string().unwrap(), "TEST123");
+        assert_status_and_body(response, 201, "TEST123");
 
-        // test for 404
-        let mut response = client
+        let response = client
             .post("/not-routed", HttpHeaders::new(), Cursor::new(""))
             .unwrap();
-        let _ = response.read_body(); // close socket
-        assert_eq!(response.status.code, 404);
+        assert_status_and_body(response, 404, "");
+
+        let response = client
+            .delete("/user/123", HttpHeaders::new(), Cursor::new(""))
+            .unwrap();
+        assert_status_and_body(response, 400, "no user: 123");
 
         // wait for server thread to finish
         let _ = h.join();
+    }
+
+    // ---------------------------------------------------------------------
+    // UTILS
+    // ---------------------------------------------------------------------
+
+    fn start_server(n: u64) -> std::thread::JoinHandle<()> {
+        thread::spawn(move || {
+            let mut app = App::new("127.0.0.1", TEST_PORT);
+
+            app.map_route(HttpMethod::Get, "/hello", |_, res| {
+                res.send(&HttpStatus::OK, HttpHeaders::new(), &b"Hello, World!"[..]);
+            });
+
+            app.map_route(HttpMethod::Post, "/api/uppercase", |mut ctx, res| {
+                let mut body = ctx.read_body().unwrap();
+                body.make_ascii_uppercase();
+                res.send(&HttpStatus::of(201), HttpHeaders::new(), &body[..]);
+            });
+
+            app.map_route(HttpMethod::Delete, "/user/:id", |ctx, res| {
+                let body = format!("no user: {}", ctx.route_params.get("id").unwrap());
+                res.send(&HttpStatus::of(400), HttpHeaders::new(), body.as_bytes());
+            });
+
+            app.serve_n(n).ok();
+        })
+    }
+
+    fn assert_status_and_body(
+        mut res: khttp::client::HttpResponse,
+        expected_status: u16,
+        expected_body: &str,
+    ) {
+        assert_eq!(res.status.code, expected_status);
+        let body = res.read_body_to_string().unwrap();
+        assert_eq!(body, expected_body);
     }
 }
