d6e2243c4d4b15d1824c9f084bf391ae7f07d8bc
karl <5895483+karlivory@users.noreply.github.com>
2025-08-04 14:15:30 +0300

    rm clones from examples/reverse_proxy

--- examples/reverse_proxy.rs
+++ examples/reverse_proxy.rs
@@ -2,32 +2,30 @@ use khttp::{Client, Headers, RequestContext, ResponseHandle, Server, Status};
 
 // hard-coded upstream
 static UPSTREAM: &str = "httpbin.org";
+static UPSTREAM_PORT: usize = 80;
 
 fn main() {
-    let mut app = Server::builder("127.0.0.1:8080").unwrap();
+    let mut app = Server::builder("127.0.0.1:9000").unwrap();
 
     // route all paths and methods to upstream
     app.fallback_route(proxy_handler);
     app.build().serve().unwrap();
 }
 
-fn proxy_handler(mut ctx: RequestContext, res: &mut ResponseHandle) -> std::io::Result<()> {
-    let mut headers = ctx.headers.clone();
+// TODO: not sure how this works with keep-alive
+fn proxy_handler(ctx: RequestContext, res: &mut ResponseHandle) -> std::io::Result<()> {
+    let (method, uri, mut headers, _, _, _, request_body) = ctx.into_parts();
     headers.set("Host", UPSTREAM.as_bytes());
 
-    let mut client = Client::new(&format!("{}:80", UPSTREAM));
-    let mut response =
-        match client.exchange(&ctx.method.clone(), ctx.uri.path(), &headers, ctx.body()) {
-            Ok(r) => r,
-            Err(e) => {
-                dbg!(e);
-                return res.send(&Status::SERVICE_UNAVAILABLE, Headers::empty(), &[][..]);
-            }
-        };
+    let mut client = Client::new(&format!("{}:{}", UPSTREAM, UPSTREAM_PORT));
+    let response = match client.exchange(&method, uri.path(), &headers, request_body) {
+        Ok(r) => r,
+        Err(e) => {
+            dbg!(e);
+            return res.send(&Status::SERVICE_UNAVAILABLE, Headers::empty(), &[][..]);
+        }
+    };
 
-    res.send(
-        &response.status.clone(),
-        &response.headers.clone(),
-        response.body(),
-    )
+    let (status, headers, response_body) = response.into_parts();
+    res.send(&status, &headers, response_body)
 }
