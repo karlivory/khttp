e1625695d04387ce538b34181b20eaccc24c6aa7
karl <5895483+karlivory@users.noreply.github.com>
2025-08-13 19:03:31 +0300

    update examples/hello_world, add examples/basics

---
+++ examples/basics.rs
@@ -0,0 +1,70 @@
+use khttp::{Headers, Method::*, PreRoutingAction, Server, Status};
+
+fn main() {
+    let mut app = Server::builder("127.0.0.1:8080").unwrap();
+
+    // route handlers are: dyn Fn(RequestContext, &mut ResponseHandle) -> io::Result<()>
+    app.route(Get, "/", |ctx, res| {
+        // Access the request via RequestContext
+        println!(
+            "Received request.\nmethod: {}\nquery: {}\nheaders:\n{}",
+            ctx.method,
+            ctx.uri.query().unwrap_or(""),
+            ctx.headers
+        );
+
+        let mut headers = Headers::new();
+        // Header names/values are Cow str and [u8] respectively,
+        // so they support both owned values and references
+        headers.add("key", b"value");
+        headers.add(String::from("owned"), b"value".to_vec());
+
+        // Send the response via ResponseHandle.
+        // Response body is of type `impl Read` (here: response is sent as &[u8]).
+        res.ok(&headers, &b"Hello, World!"[..])
+    });
+
+    app.route(Post, "/uppercase", |mut ctx, res| {
+        // ctx.body() implements Read but it also has convenience methods ::string() and ::vec()
+        let body = ctx.body().string().unwrap_or_default();
+        let response_body = body.to_ascii_uppercase();
+        // "date" and "content-length" headers are added automatically
+        res.ok(Headers::empty(), response_body.as_bytes())
+    });
+
+    // Routing supports named parameters
+    app.route(Get, "/user/:id", |ctx, res| {
+        let user_id: u64 = match ctx.params.get("id").unwrap().parse() {
+            Ok(id) => id,
+            Err(_) => {
+                return res.send(&Status::BAD_REQUEST, Headers::empty(), &b"invalid id"[..]);
+            }
+        };
+        res.ok(Headers::empty(), format!("{}", user_id).as_bytes())
+    });
+
+    // All other requests are handled via the fallback_route
+    app.fallback_route(|_, res| res.send(&Status::NOT_FOUND, Headers::empty(), &b"not found"[..]));
+
+    // Fine-tuning
+    app.thread_count(20);
+    app.max_request_head_size(16 * 1024);
+    // Pre-routing-hooks runs after request-head is parsed, right before routing
+    app.pre_routing_hook(|req, res, conn| {
+        if conn.index() > 100 {
+            let _ = res.send(&Status::of(429), Headers::close(), std::io::empty());
+            return PreRoutingAction::Drop;
+        }
+        if req.http_version == 0 {
+            let _ = res.send(&Status::of(505), Headers::close(), std::io::empty());
+            return PreRoutingAction::Drop;
+        }
+        if matches!(req.method, Custom(_)) {
+            let _ = res.send(&Status::of(405), Headers::close(), std::io::empty());
+            return PreRoutingAction::Drop;
+        }
+        PreRoutingAction::Proceed
+    });
+
+    app.build().serve_epoll().unwrap();
+}
--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,24 +1,9 @@
-use khttp::{Headers, Method::*, Server, Status};
+use khttp::{Headers, Method::*, Server};
+
+static HELLO_WORLD: &[u8] = b"Hello, World!";
 
 fn main() {
     let mut app = Server::builder("127.0.0.1:8080").unwrap();
-    app.thread_count(20);
-
-    // custom fallback_route
-    app.fallback_route(|_, r| {
-        r.send(
-            &Status::NOT_FOUND,
-            Headers::empty(),
-            &b"404 - not found"[..],
-        )
-    });
-
-    // GET http://localhost:8080 should respond with "Hello, World!"
-    app.route(Get, "/", |_, r| {
-        let mut headers = Headers::new();
-        headers.add("Server", b"khttp");
-        r.ok(&headers, &b"Hello, World!"[..])
-    });
-
+    app.route(Get, "/", |_, res| res.ok(Headers::empty(), HELLO_WORLD));
     app.build().serve_epoll().unwrap();
 }
