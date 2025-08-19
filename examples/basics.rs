7a77dcb4e5ce319b534736bdbb60704a200cdfc4
karl <5895483+karlivory@users.noreply.github.com>
2025-08-19 14:01:56 +0300

    update readme, example/basics

--- examples/basics.rs
+++ examples/basics.rs
@@ -6,15 +6,12 @@ fn main() {
     // GET route: hello world!
     app.route(Get, "/", |ctx, res| {
         println!(
-            "Request: method={}, query={}, headers=\n{}",
-            ctx.method,
-            ctx.uri.query().unwrap_or(""),
-            ctx.headers
+            "method={}, uri={}, headers=\n{}",
+            ctx.method, ctx.uri, ctx.headers
         );
 
         let mut headers = Headers::new();
         headers.add("key", b"value");
-        headers.add(String::from("owned"), b"value".to_vec());
 
         // Response body is just `impl Read` (here: &[u8]).
         res.ok(&headers, &b"Hello, World!"[..])
@@ -22,14 +19,12 @@ fn main() {
 
     // POST route: uppercases request body
     app.route(Post, "/uppercase", |mut ctx, res| {
-        // ctx.body() implements Read
-        let body = ctx.body().string().unwrap_or_default();
+        let body = ctx.body().string().unwrap_or_default(); // ctx.body() is `impl Read`
         let response_body = body.to_ascii_uppercase();
-        // "date" and "content-length" headers are automatically added
         res.ok(Headers::empty(), response_body.as_bytes())
     });
 
-    // Routing: named parameter
+    // Routing: named parameters and wildcards are supported
     app.route(Get, "/user/:id", |ctx, res| {
         let user_id: u64 = match ctx.params.get("id").unwrap().parse() {
             Ok(id) => id,
@@ -37,16 +32,12 @@ fn main() {
         };
         res.ok(Headers::empty(), format!("{}", user_id).as_bytes())
     });
-    // Routing: wildcard (matches one segment)
     app.route(Get, "/api/v1/*", |_, r| r.ok(Headers::empty(), &[][..]));
-    // Routing: double-wildcard (matches any number of segments)
     app.route(Get, "/static/**", |_, r| r.ok(Headers::empty(), &[][..]));
-
-    // Fallback_route
-    app.fallback_route(|_, r| r.send(&Status::NOT_FOUND, Headers::empty(), &b"404"[..]));
 
     // Fine-tuning
     app.thread_count(20);
+    app.fallback_route(|_, r| r.send(&Status::NOT_FOUND, Headers::empty(), &b"404"[..]));
     app.max_request_head_size(16 * 1024);
     app.pre_routing_hook(|req, res, conn| {
         if conn.index() > 100 {
@@ -64,6 +55,6 @@ fn main() {
         PreRoutingAction::Proceed
     });
 
-    // `serve_epoll` is enabled via "epoll" feature
+    // `serve_epoll` is available via the "epoll" feature
     app.build().serve_epoll().unwrap();
 }
